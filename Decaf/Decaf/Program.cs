5d9ab57ad8083d5488ba6742bc3e3b8dbe5135e8
unknown <Andrew Browne@.(none)>
2009-04-01 20:28:23 +0200

    Created Decaf Project

---
+++ Decaf/Decaf/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Decaf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
